072783c57f4c63d4b96ad07efe87e625de29891f
abdur <pcabdur3@gmail.com>
2026-08-27 09:58:12 +0530

    feat: add Python hello world basics

---
+++ 01-basics/01-hello-python.py
@@ -0,0 +1,15 @@
+print(" Hello man wassup")
+print (" we are learning python from scrach bruh ")
+
+print("=====================")
+print("\t profile")
+print("=====================")
+name = "Abdur"
+age= 20
+learning ="python"
+goal = "to master python"
+print( "Name :",name)
+print("Age:",age)
+print("learning:",learning)
+print("Goal:",goal)
+print("=====================")
