8e8f116c72841e70d7b9bf178471758ef58b91b8
abdur <pcabdur3@gmail.com>
2026-08-27 10:20:21 +0530

    feat: add Python input practice

---
+++ 01-basics/05-input.py
@@ -0,0 +1,19 @@
+name=input("what is ur name burh :")
+print( name)
+# this store as int bruh so 
+age= input(" what ur age my boy:")
+print("u are uncle bruh :",age)
+
+
+height= int( input(" give me ur height dog:"))
+print("oyu are short bruh hshsh:",height)
+
+
+country = input("Country: ")
+language = input("Favorite programming language: ")
+print(country)
+print(language)
+
+bill=float( input("What is the bill amount?"))
+people=int( input("how many gng:"))
+print("Each person pays: ",bill/people)
