617a16badbb0a4d1df6638130f5433c6a2e66cef
abdur <pcabdur3@gmail.com>
2026-08-27 13:08:02 +0530

    feat: add Python conditionals practice

---
+++ 01-basics/07-conditionals.py
@@ -0,0 +1,43 @@
+#    IF 
+age = 20
+if age>=18:
+    print(" you re a major hahah pay tax bruh ")
+    print("bruh vote for nota haha")
+else:
+    print("go to pre shcool hahah")
+
+
+height=int(input("bruh how tall are u bruh let me knw :"))
+if height>184:
+    print("bro mogs lol")
+elif height<183 or height>=175:
+    print( "u arent 6'0 hahah ")
+else:
+    print("u are short")
+
+
+password = "bruh121"
+
+if password == "bruh121":
+    print("Login successful")
+else:
+    print("Wrong password")
+
+
+# odd or even 
+num=int( input("enter a number to find odd or even "))
+if num%2==0:
+    print("even")
+else:
+    print("odd")
+
+
+# lets do a loggin system bruh hshs
+
+name="abdur"
+print(name)
+pasws=int(input("enter pass:"))
+if pasws=="212":
+    print("succes")
+else:
+    print("bruh gtfo ")
