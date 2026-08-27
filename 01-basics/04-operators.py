68231581bb27bb143fcf56a073885b71b554b128
abdur <pcabdur3@gmail.com>
2026-08-27 09:58:26 +0530

    feat: add Python operators practice

---
+++ 01-basics/04-operators.py
@@ -0,0 +1,57 @@
+x = 10
+y = 3
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(x ** y)
+print(x == 10)
+age=20
+print(age>10 and age<30)
+#      AND
+print(True and True)    # True
+print(True and False)   # False
+print(False and True)   # False
+print(False and False)  # False
+#   OR
+print(True or False)    # True
+print(False or True)    # True
+print(False or False)   # False
+#   NOR
+print(not True)
+print(not False)
+
+
+# odd or even 
+ 
+num =101
+print(num%2)
+
+print(x + y * 2)
+
+print((x + y) * 2)
+
+
+# summ
+
+x = 10
+y = 3
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(x ** y)
+
+print(x > y)
+print(x == y)
+print(x != y)
+
+print(x > 5 and y < 5)
+print(x > 20 or y < 5)
+print(not(x > y))
