028422657d563b9386877e12566336f7180fc35d
abdur <pcabdur3@gmail.com>
2026-08-27 09:58:20 +0530

    feat: add Python data types practice

---
+++ 01-basics/03-data-types.py
@@ -0,0 +1,32 @@
+x = 10
+y = 10.0
+z = "10"
+a = True
+b = None
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(b))
+
+# 2 
+
+a = 100
+b = 100.0
+c = "100"
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+#3
+x = "10"
+y = "20"
+
+print(x + y)
