a54371a9b3b008ef8714a7956b90a7addb4dd248
abdur <pcabdur3@gmail.com>
2026-08-27 12:50:25 +0530

    feat: add Python type conversion practice

---
+++ 01-basics/06-type-conversion.py
@@ -0,0 +1,16 @@
+x = "10"
+y = "20"
+
+a = int(x)
+b = int(y)
+
+print(a + b)
+print(x + y)
+
+print(type(a))
+print(type(x))
+
+
+C= float( input("enter temp in celci:"))
+F = (C * 9/5) + 32
+print("farinhite",F)
