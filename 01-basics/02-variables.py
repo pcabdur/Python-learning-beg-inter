ff90bd8d3035a318a8ccf433744b8b4fe5fc9326
abdur <pcabdur3@gmail.com>
2026-08-27 09:57:44 +0530

    feat: add Python variables practice

---
+++ 01-basics/02-variables.py
@@ -0,0 +1,56 @@
+name = "Abdur"
+age = 20
+height = 185
+is_student = True
+
+print ( name,age,height,is_student)
+age =22
+print(age)
+## next
+x=10
+y=22
+print(x+y)
+## next 
+x="10"
+y="22"
+print(x+y)
+
+name=" pca"
+age=19
+height=185
+country="IND"
+favorite_number=5
+is_learning_python=True
+print(name,
+age,
+height,
+country,
+favorite_number,
+is_learning_python)
+age+=5
+favorite_number*=2
+print(age,favorite_number)
+
+#  TYPES
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+x=100
+y=-50
+z=0
+
+print(x,y,z)
+print(type(x),type(y),type(z))
+
+# float
+price=99.55
+print(price)
+print(type(price))
+
+# string
+message="bruh wessup"
+message_sing="bruh wessup"
+print( message)
+print(message_sing)
